dbbea5dbefd218c06ab59d70edbeea3a078d0909
Francisco Portillo <francisco.portillo@dextra-sw.com>
2013-12-02 17:41:19 -0200

    only call ajax when panels are expanding

--- js/ajaxLoad.js
+++ js/ajaxLoad.js
@@ -1,7 +1,11 @@
 function atualizaAjax(event, ui) {
+  var baseUrl = "cgi-bin/";
+  var script = $(ui.newHeader).text();
+  if (script == "") return;
+  var scriptUrl = baseUrl + script;
   $.ajax({
     type: "GET",
-    url: "cgi-bin/teste.py",
+    url: scriptUrl,
     beforeSend: function() {alert('ajax before send')},
     complete: function(xhr, textStatus) {completouAjax(xhr, textStatus, event, ui)}
   });
